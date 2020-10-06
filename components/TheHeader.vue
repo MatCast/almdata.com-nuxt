3ba2605c2ecc45dbfff8a354cb5f8ae2e21e919e
MatCast <32680270+MatCast@users.noreply.github.com>
2020-10-06 20:35:28 +0200

    created TheHeader component

---
+++ components/TheHeader.vue
@@ -0,0 +1,57 @@
+<template>
+  <b-navbar>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+          alt="Lightweight UI components for Vue.js based on Bulma"
+        />
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item href="#"> Home </b-navbar-item>
+      <b-navbar-item href="#"> Documentation </b-navbar-item>
+      <b-navbar-dropdown label="Info">
+        <b-navbar-item href="#"> About </b-navbar-item>
+        <b-navbar-item href="#"> Contact </b-navbar-item>
+      </b-navbar-dropdown>
+    </template>
+
+    <template slot="end">
+      <b-navbar-item
+        v-for="(item, key) of items"
+        :key="key"
+        tag="nuxt-link"
+        :to="item.to"
+      >
+        {{ item.title }}
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' },
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' },
+        },
+        {
+          title: 'Inspire',
+          icon: 'blog',
+          to: { name: 'blog' },
+        },
+      ],
+    }
+  },
+}
+</script>
