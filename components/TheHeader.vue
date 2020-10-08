aad215e060c677b2a37347711f410afc7dc244c9
MatCast <gozyssj4@gmail.com>
2020-10-08 11:22:20 +0200

    style changes

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -3,10 +3,7 @@
     <b-navbar>
       <template slot="brand">
         <b-navbar-item tag="nuxt-link" :to="{ path: '/' }">
-          <img
-            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-            alt="Lightweight UI components for Vue.js based on Bulma"
-          />
+          <strong>almdata.com</strong>
         </b-navbar-item>
       </template>
 
